ecc8e62b5f7ec99bd50f040e485c18ec91cebf28
johnasblasco <johnaslblasco@gmail.com>
2025-07-29 21:19:52 +0800

    feat: Trigger onAdmit callback after successful admission

--- src/dashboard/features/admission/pages/Admission.jsx
+++ src/dashboard/features/admission/pages/Admission.jsx
@@ -118,6 +118,7 @@ const Admission = ({ onAdmit }) => {
                                         icon: "success",
                                         confirmButtonText: "OK"
                                     });
+                                    onAdmit();
 
                                 })
                                 .catch((error) => {
