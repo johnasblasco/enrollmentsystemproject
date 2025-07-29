2cc691170c46b3fd0734cea4920451315e99855b
johnasblasco <johnaslblasco@gmail.com>
2025-07-29 23:35:10 +0800

    feat: swal

--- src/auth/pages/RegisterTwo.jsx
+++ src/auth/pages/RegisterTwo.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect, useContext } from 'react'
 import { Link, useNavigate } from 'react-router-dom'
 import axios from 'axios'
+import Swal from 'sweetalert2'
 
 import { AdmissionContext } from '@/globalContexts/AdmissionContext'
 
