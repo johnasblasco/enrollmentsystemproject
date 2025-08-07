563aee980073b7a119e3b50a3b7ad616fd201f42
johnasblasco <johnaslblasco@gmail.com>
2025-08-07 21:49:16 +0800

    frontend

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,8 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 import path from 'path'
-// https://vite.dev/config/
 export default defineConfig({
-  base: `/enrollment/`,
+  base: `/frontend/`,
   plugins: [react(),
   tailwindcss()
   ],
